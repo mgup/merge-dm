6f523cafdf149ba24beba1777312f53a3dcfbe8a
Vitaly Shlyaga <storkvist@storkvist.net>
2015-02-04 13:13:58 +0300

    Парсинг всех файлов.

--- run.rb
+++ run.rb
@@ -1,37 +1,21 @@
 require 'csv'
 
-def generate_doc_hash(row)
-  student_id = row[39]
-  return nil if student_id.nil?
-  
-  # return nil if row[34].nil?
-  # diploma_number = row[34].split(' ')[1]
-  # return nil if diploma_number.nil?
-  
-  {
-    student_id: row[39],
-    series: '107705' #,
-    # number: diploma_number
-  }
-end
-
 docs = {}
 
-index = 1
 Dir.glob('dbs/*.mdb') do |filename|
   `mdb-export -HQ -d $ -R @ #{filename} dmDoc | tr '\r' ' ' | tr '\n' ' ' > dmDoc.csv`
   
   # j = 1
   CSV.foreach('dmDoc.csv', col_sep: '$', row_sep: '@', quote_char: '§') do |row|
     student_id = row[39]
-    
-    hash = generate_doc_hash(row)
-    
-    unless hash.nil?
+    unless student_id.nil?
       if docs.has_key?(student_id)
-        docs[student_id].push(generate_doc_hash(row))
+        docs[student_id][:doc].push(row)
       else
-        docs[student_id] = [generate_doc_hash(row)]
+        docs[student_id] = {
+          doc: [row],
+          student: []
+        }
       end
     end
               
@@ -42,12 +26,13 @@ Dir.glob('dbs/*.mdb') do |filename|
   
   `mdb-export -HQ -d $ -R @ #{filename} dmStudent | tr '\r' ' ' | tr '\n' ' ' > dmStudent.csv`
   CSV.foreach('dmStudent.csv', col_sep: '$', row_sep: '@', quote_char: '§') do |row|
-    # puts row[39]
-    # fail '123'
+    student_id = row[0]
+    if docs.has_key?(student_id)
+      docs[student_id][:student].push(row)
+    end
   end
   `rm dmStudent.csv`
   
-  index += 1
   # break
 end
 
